e1402835cd4a93d123f3d45257eeb2da49ae9980
Stephen <Joyce@192.168.1.146>
2018-04-22 18:16:59 -0400

    Class 1

---
+++ DrawFace/src/DrawFace.java
@@ -0,0 +1,27 @@
+import java.awt.Color;
+	import java.awt.Graphics;
+	
+	import javax.swing.JPanel;
+	
+public class DrawFace extends JPanel 	
+	{
+		public void paintComponent(Graphics g)
+		{
+			super.paintComponent(g);
+			
+			g.setColor(Color.YELLOW);
+			g.fillOval(10,  10,  200,  200);
+			
+			g.setColor(Color.BLUE);
+			g.fillOval(55,  55, 20,  30);
+			g.fillOval(135,  55,  20,  30);
+			g.setColor(Color.PINK);
+			g.fillOval(50,  110,  120,  60);
+			
+			g.setColor(Color.YELLOW);
+			g.fillRect(50,  100,  120,  30);
+			g.fillOval(50,  120,  120,  40);
+			
+		}
+	}
+
